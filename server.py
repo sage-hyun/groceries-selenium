0962faae5a3139fe7882f5a866b9a1cde9308b04
sage-hyun <dan06052@naver.com>
2022-03-25 23:37:47 +0900

    !BREAKING CHANGE(API): Add /scrape
    - scrape() saves data to mongoDB
    - index() reads data from mongoDB

--- server.py
+++ server.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
+import pymongo
+from flask import Flask, render_template, jsonify
 from scraping import get_groceries_data
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+DB_URI = os.getenv('DB_URI')
+
+client = pymongo.MongoClient(DB_URI)
+db = client.test001     # database name
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    groceries_data = get_groceries_data()
+    # recieve data from mongoDB
+    groceries_data = list(db.recipt.find())
+
     return render_template("index.html", data=groceries_data)
 
+
+@app.route("/scrape")
+def scrape():
+    groceries_data = get_groceries_data()   # scraping
+
+    # add data to mongoDB
+    db.recipt.insert_many(groceries_data)
+    return jsonify({"code": 0, "msg": "Storage completed"})
 
 
 if __name__ == '__main__':
