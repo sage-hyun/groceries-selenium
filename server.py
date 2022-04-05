51b42279c06d2832361a814ccc06d8f94e8f6e93
sage-hyun <dan06052@naver.com>
2022-04-06 01:09:08 +0900

    Feat: Calc similarity b/w predicted day & today
    prediction(similarity) = 1 / ( | diff |  + 1) * 100

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from scraping import get_groceries_data
 from data_processing import calculate_values
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv(verbose=True)
 DB_URI = os.getenv('DB_URI')
@@ -17,7 +18,16 @@ app = Flask(__name__)
 def index():
     # recieve data from mongoDB
     groceries_data = list(db.recipt.find())
-    
+
+    # items bought once are unimportant
+    groceries_data = [x for x in groceries_data if not x['count']==1 ]
+
+    # calculate prediction
+    for elem in groceries_data:        
+        predicted_day = datetime.strptime(elem['predicted_day'], '%Y.%m.%d')
+        diff = datetime.now() - predicted_day
+        elem['prediction'] = (1 / (abs(diff.days) + 1)) * 100
+
     # sort by counts
     groceries_data_by_count = sorted(groceries_data, key=lambda elem: elem['count'], reverse=True)
 
